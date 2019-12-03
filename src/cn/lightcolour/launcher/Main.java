d65a6bb5a0c670541a1e8a4b5e2090d339c0fa90
Lightcolour <32991121+Lightcolour-666@users.noreply.github.com>
2019-12-03 20:58:51 +0800

    JSON

--- src/cn/lightcolour/launcher/Main.java
+++ src/cn/lightcolour/launcher/Main.java
@@ -5,8 +5,7 @@ import java.io.*;
 public class Main
 {
     public static void main(final String[] args) throws IOException {
-        LauncherScript.ShowLibraries("C:\\Users\\lightcolour\\Desktop\\HMCL\\.minecraft\\versions\\1.8.8\\1.8.8.json");
         LauncherScript.write("C:\\Users\\lightcolour\\Desktop","1.bat");
-//        LauncherScript.runScript("C:\\Users\\lightcolour\\Desktop","1.bat");
+        LauncherScript.runScript("C:\\Users\\lightcolour\\Desktop","1.bat");
     }
 }
