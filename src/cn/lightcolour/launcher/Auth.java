77d2e9b9106eef6da9a6f329a74253e4f445007b
Lightcolour <32991121+Lightcolour-666@users.noreply.github.com>
2019-12-03 20:54:52 +0800

    README

--- src/cn/lightcolour/launcher/Auth.java
+++ src/cn/lightcolour/launcher/Auth.java
@@ -1,24 +1,20 @@
 package cn.lightcolour.launcher;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
+
 
 public class Auth {
 
     public static String Check() throws IOException {
         String url = "https://status.mojang.com/check";
-        String s = getpost(url);
+        String s = Utils.requset(url);
         String re = "";
-        s = getSubString(s,"[","]");
+        s = Utils.getSubString(s,"[","]");
         String[] arr = s.split(",");
         for (int i = 0;arr.length > i;i++) {
-            String[] a = getSubString(arr[i],"{","}").split(":");
-            String left = getSubString(a[0],"\"","\"");
-            String right = getSubString(a[1],"\"","\"");
+            String[] a = Utils.getSubString(arr[i],"{","}").split(":");
+            String left = Utils.getSubString(a[0],"\"","\"");
+            String right = Utils.getSubString(a[1],"\"","\"");
             re += "Server:" + left + "," + "Check:" + right + "\n";
         }
         return re;
@@ -26,60 +22,17 @@ public class Auth {
 
     public static String getUUID(String id) throws IOException {
         String url = "https://api.mojang.com/users/profiles/minecraft/" + id;
-        String s = getpost(url);
-        s = getSubString(s,"{","}");
+        String s = Utils.requset(url);
+        s = Utils.getSubString(s,"{","}");
         String[] arr = s.split(",");
         for (int i = 0;arr.length > i;i++) {
             String[] a = arr[i].split(":");
             String left = a[0];
             String right = a[1];
             if(left.equals("\"id\"")) {
-                return getSubString(right,"\"","\"");
+                return Utils.getSubString(right,"\"","\"");
             }
         }
         return "";
-    }
-
-
-    private static String getpost(String url) throws IOException {
-        String result = "";
-        BufferedReader in = null;
-        String urlNameString = url;
-        URL realUrl = new URL(urlNameString);
-        URLConnection connection = realUrl.openConnection();
-        connection.setRequestProperty("accept", "*/*");
-        connection.setRequestProperty("connection", "Keep-Alive");
-        connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-        connection.connect();
-
-        in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = in.readLine()) != null) {
-            result += line;
-        }
-
-        return result;
-    }
-
-    private static String getSubString(String text, String left, String right) {
-        String result = "";
-        int zLen;
-        if (left == null || left.isEmpty()) {
-            zLen = 0;
-        } else {
-            zLen = text.indexOf(left);
-            if (zLen > -1) {
-                zLen += left.length();
-            } else {
-                zLen = 0;
-            }
-        }
-        int yLen = text.indexOf(right, zLen);
-        if (yLen < 0 || right == null || right.isEmpty()) {
-            yLen = text.length();
-        }
-        result = text.substring(zLen, yLen);
-        return result;
-
     }
 }
---
+++ src/cn/lightcolour/launcher/Utils.java
@@ -0,0 +1,63 @@
+package cn.lightcolour.launcher;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Utils {
+    public static String requset(String url) throws IOException {
+        String result = "";
+        BufferedReader in = null;
+        String urlNameString = url;
+        URL realUrl = new URL(urlNameString);
+        URLConnection connection = realUrl.openConnection();
+        connection.setRequestProperty("accept", "*/*");
+        connection.setRequestProperty("connection", "Keep-Alive");
+        connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+        connection.connect();
+
+        in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            result += line;
+        }
+        return result;
+    }
+
+    public static String getSubString(String text, String left, String right) {
+        String result = "";
+        int zLen;
+        if (left == null || left.isEmpty()) {
+            zLen = 0;
+        } else {
+            zLen = text.indexOf(left);
+            if (zLen > -1) {
+                zLen += left.length();
+            } else {
+                zLen = 0;
+            }
+        }
+        int yLen = text.indexOf(right, zLen);
+        if (yLen < 0 || right == null || right.isEmpty()) {
+            yLen = text.length();
+        }
+        result = text.substring(zLen, yLen);
+        return result;
+
+    }
+
+    public static String txtString(File file){
+        StringBuilder result = new StringBuilder();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));//构造一个BufferedReader类来读取文件
+            String s = null;
+            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+                result.append(System.lineSeparator()+s);
+            }
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+}
